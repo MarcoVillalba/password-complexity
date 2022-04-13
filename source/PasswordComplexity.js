5318b7633c75f888741b87d4f8cc8fa35ea76f81
mvillalba <marco.villalba.012@gmail.com>
2022-04-13 15:08:50 -0300

    entrevista mv
    entrevista mv

--- source/PasswordComplexity.js
+++ source/PasswordComplexity.js
@@ -4,6 +4,57 @@ const PasswordComplexityValues = {
     EXCELLENT: 'excellent',
 }
 
-const calculateComplexity = password => 'Good Luck!'
+//const calculateComplexity = password => 'Good Luck!'
+
+
+const calculateComplexity = (password) =>{
+    let res = false;
+    let a = false;
+    let b =  false;
+    let c = false;
+    let v_letras = 0;
+    let v_num = 0;
+
+
+    // - a) Regla de mínimo de caracteres (ejemplo: Longitud mínima 8 caracteres).
+    // - b) Regla de que contenga (al menos) 2 números siempre y cuando también contenga letras.
+    // - c) Implementar una función que:
+    //   - Si cumple con las 2 reglas → es Excelente
+    //   - Si cumple con sólo 1 → es Buena
+    //   - Si no cumple con ninguna → es Débil
+
+    if(password.length >= 8)
+       a = true;
+
+    
+       //console.log('antes for')
+
+    for (let i = 0; i <= password.length; i ++)
+    {
+        //console.log(password[i])
+
+        if(isNaN(password[i])){
+           // console.log('dentro del if')
+            v_letras = v_letras +1;
+        }           
+        else
+            v_num = v_num +1;
+    }
+
+    if( (v_num > 1 && v_letras != 0) && a)
+        b = true;
+
+
+
+    // Si cumple con sólo 1 → es Buena
+    if (a && b){
+        return PasswordComplexityValues.EXCELLENT;
+    }else{
+        if(a || b)
+            return PasswordComplexityValues.GOOD;
+        else
+            return PasswordComplexityValues.WEAK;
+    }      
+} 
 
 module.exports = { calculateComplexity, PasswordComplexityValues }
--- source/__tests__/PasswordComplexity.spec.js
+++ source/__tests__/PasswordComplexity.spec.js
@@ -1,7 +1,7 @@
 const { calculateComplexity, PasswordComplexityValues } = require("../PasswordComplexity")
 
 describe('Password complexity tests', () => {
-    it('should be excellent', () => {
+    it('should be excellent current', () => {
         const complexity = calculateComplexity('password123')
         expect(complexity).toEqual(PasswordComplexityValues.EXCELLENT)
     })
@@ -14,5 +14,10 @@ describe('Password complexity tests', () => {
     it('should be weak', () => {
         const complexity = calculateComplexity('pass')
         expect(complexity).toEqual(PasswordComplexityValues.WEAK)
-    })    
+    })   
+    
+    // it('should be good', () => {
+    //     const complexity = calculateComplexity('123456789')
+    //     expect(complexity).toEqual(PasswordComplexityValues.GOOD)
+    // })
 })
